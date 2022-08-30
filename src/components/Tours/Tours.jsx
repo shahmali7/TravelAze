e5dbf5ace1c2036589f4de2c3dbb18252373a4d5
ORBIT-CS <shahmali.mustafayev.01@gmail.com>
2022-08-31 00:22:41 +0200

    was changed

--- src/components/Tours/Tours.jsx
+++ src/components/Tours/Tours.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
 import { Cities } from '../../FakeApi/Cities'
 import './Tours.css'
+import { BsSuitHeart } from 'react-icons/bs'
+
 const Tours = () => {
   return (
     <div name='tours' className='ContainerTours'>
         <div className='headTour'>
-            <h2>Our Tours</h2>
+            <p style={{color:'red'}}>Where to go</p>
+            <h3 style={{color:'blue'}}>in Azerbaijan</h3>
         </div>
         <div className='grid-container'>
+                    <div  className='imagesGrid'>
             {Cities.map((item)=>{
                 return(
-                    <div key={item.id} >
-                        {/* <h3>{item.name}</h3> */}
+                    <div key={item.id} className='imagesDiv'>
                         <img src={item.image} className={item.className} alt=''/>
+                        <div className='overlay'>
+                            <span><BsSuitHeart/></span>
+                            <div>
+                                <h3>{item.name}</h3>
+                                <button>More</button>
+                            </div>
+                        </div>
                     </div>
                 )
             })}
+                    </div>
         </div>
     </div>
   )
